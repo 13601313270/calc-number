ca4e9b05582633d4c65d72b1a3c3ff5ced3182ba
whr <wang.whr@alibaba-inc.com>
2023-11-28 09:56:16 +0800

    基本运算

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import split from "./split";
 class runObj {
   constructor(type, data) {
     this.type = type;
-    this.data = data;
+    this.data = data.map(v => {
+      if (v instanceof Array) {
+        let runObjItem = new runObj(v[1], [v[0], v[2]]);
+        return runObjItem.run();
+      } else {
+        return v;
+      }
+    });
   }
   run() {
+    const BigNumber = require('bignumber.js')
     if (this.type === '+') {
-      const BigNumber = require('bignumber.js')
       return BigNumber.sum(this.data[0], this.data[1]).toNumber()
     } else if (this.type === '-') {
-
+      const temp = new BigNumber(this.data[0])
+      return temp.minus(this.data[1]).toNumber()
+    } else if (this.type === '*') {
+      const temp = new BigNumber(this.data[0])
+      return temp.multipliedBy(this.data[1]).toNumber()
+    } else if (this.type === '/') {
+      const temp = new BigNumber(this.data[0])
+      return temp.div(this.data[1]).toNumber()
+    } else {
+      throw new Error('运算类型不存在' + this.type)
     }
   }
 }
@@ -36,20 +52,33 @@ function numberCalc(runStr) {
   let resultArr = split(runStr);// '1+2'    =>   ['1', '+', '2']
   // 第二步
   // ['1', '+', '2'] => { runType: '+', data: [{runType: 'number', data: 1},'2']}
-  const temp = [[]];
+  let temp = [[]];
   // 1 + 1 * 2 - 3 * 2
   // [1 ,+ ,1, * ,2, - ,3, * ,2]
   // [[1] , [+] ,[1, * ,2], [-] ,[3, * ,2]]
   for (let i = 0; i < resultArr.length; i++) {
-    if (resultArr[i].match(/\d+/) || ['*', '/'].includes(resultArr[i])) {
+    if (resultArr[i].match(/\d+/)) {
       temp[temp.length - 1].push(resultArr[i]);
+      // if (temp[temp.length - 1].length === 1) {
+      //   // 1+2*3/2+2*4
+      //   // 1+(2*3/2)+(2*4)
+      //   temp[temp.length - 1] = parseFloat(temp[temp.length - 1][0])
+      // }
+    } else if (['*', '/'].includes(resultArr[i])) {
+      if (temp[temp.length - 1].length === 1) {
+        temp[temp.length - 1].push(resultArr[i]);
+      } else {
+        temp[temp.length - 1] = [temp[temp.length - 1]]
+        temp[temp.length - 1].push(resultArr[i]);
+      }
     } else if (['+', '-'].includes(resultArr[i])) {
       if (temp[temp.length - 1].length === 1) {
-        // 1+2*3/2+2*4
-        // 1+(2*3/2)+(2*4)
         temp[temp.length - 1] = parseFloat(temp[temp.length - 1][0])
       }
       temp.push([])
+      if (temp.length > 2) {
+        temp = [[temp[0], temp[1], temp[2]], []]
+      }
       temp[temp.length - 1] = resultArr[i];
       temp.push([])
     }
@@ -59,9 +88,15 @@ function numberCalc(runStr) {
     // 1+(2*3/2)+(2*4)
     temp[temp.length - 1] = parseFloat(temp[temp.length - 1][0])
   }
+  if (temp.length === 1) {
+    temp = temp[0]
+  }
+  console.log(temp)
   let runObjItem;
-  if (['+', '-'].includes(temp[1])) {
+  if (['+', '-', '*', '/'].includes(temp[1])) {
     runObjItem = new runObj(temp[1], [temp[0], temp[2]]);
+  } else {
+    throw new Error('结构不存在' + temp[1])
   }
   return runObjItem.run()
 }
